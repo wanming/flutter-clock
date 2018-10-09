e3e5cdb5a259b92acf06960404fc71a3be70ca68
TomWan <wanmingtom@gmail.com>
2018-10-10 00:10:46 +0800

    feat: weather

--- lib/common.dart
+++ lib/common.dart
@@ -1,3 +1,8 @@
+import 'package:location/location.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const CLOCK_INTERVAL = Duration(microseconds: 200);
 
 String pad0(int num) {
@@ -7,7 +12,7 @@ String pad0(int num) {
   return num.toString();
 }
 
-const ChineseWeekDays = <int, String> {
+const ChineseWeekDays = <int, String>{
   1: '一',
   2: '二',
   3: '三',
@@ -16,3 +21,28 @@ const ChineseWeekDays = <int, String> {
   6: '六',
   7: '日',
 };
+
+Future<List<double>> getLocation() async {
+  var location = new Location();
+  var currentLocation = await location.getLocation();
+  return [currentLocation['longitude'], currentLocation['latitude']];
+}
+
+const BASE_URL =
+    'https://free-api.heweather.com/s6/weather/forecast?key=7d5df9c2d3d94216b631f1ff4c277b5f&location=';
+Future<Map<String, String>> getWeather(List<double> location) async {
+  var url = '$BASE_URL${location[0]},${location[1]}';
+  var response = await http.get(url);
+  Map body = json.decode(response.body);
+  // TODO: Use cond_text_n if is in night.
+  var weather = '${body['HeWeather6'][0]['daily_forecast']['cond_txt_d']}';
+  var high = '${body['HeWeather6'][0]['daily_forecast']['tmp_max']}';
+  var low = '${body['HeWeather6'][0]['daily_forecast']['tmp_min']}';
+  return <String, String>{weather: weather, high: high, low: low};
+}
+
+Future<String> getWeatherString() async {
+  var location = await getLocation();
+  var weather = await getWeather(location);
+  return '${weather['weather']}，${weather['low']}~${weather['high']}';
+}
--- lib/themes/week.dart
+++ lib/themes/week.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../theme_base.dart';
 import '../common.dart';
+import 'dart:async';
 
 class Week extends ClockBaseStatefulWidget {
   Week({Key key}) : super(key: key);
@@ -9,6 +10,7 @@ class Week extends ClockBaseStatefulWidget {
   _WeekState createState() {
     var state = new _WeekState();
     state.startClock();
+    state.startFetchWeather();
     return state;
   }
 }
@@ -16,14 +18,16 @@ class Week extends ClockBaseStatefulWidget {
 class _WeekState extends ClockBaseState<Week> {
   _WeekState() : super();
 
+  String weather = '';
+
   @override
   Widget build(BuildContext context) {
     var deviceWith = getDeviceSize(context).height;
-    var _timeStyle = new TextStyle(
+    var timeStyle = new TextStyle(
         fontSize: deviceWith * 0.4, color: Color.fromARGB(255, 240, 240, 240));
-    var _dateStyle = new TextStyle(
+    var dateStyle = new TextStyle(
         fontSize: deviceWith * 0.08, color: Color.fromARGB(255, 194, 194, 194));
-    var _weatherStyle = new TextStyle(
+    var weatherStyle = new TextStyle(
         fontSize: deviceWith * 0.08, color: Color.fromARGB(255, 194, 194, 194));
     return GestureDetector(
         onTap: () {
@@ -32,32 +36,48 @@ class _WeekState extends ClockBaseState<Week> {
         },
         child: Scaffold(
             body: new Container(
-          padding: EdgeInsets.fromLTRB(_timeStyle.fontSize / 2, 0.0, 0.0, 0.0),
+          padding: EdgeInsets.fromLTRB(timeStyle.fontSize / 2, 0.0, 0.0, 0.0),
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new Container(
                 padding:
-                    EdgeInsets.fromLTRB(_dateStyle.fontSize * 1, 0.0, 0.0, 0.0),
+                    EdgeInsets.fromLTRB(dateStyle.fontSize * 1, 0.0, 0.0, 0.0),
                 child: Row(children: <Widget>[
                   new Text(
                       '${now.month}月${now.day}日，星期${ChineseWeekDays[now.weekday]}',
-                      style: _dateStyle)
+                      style: dateStyle)
                 ]),
               ),
               new Row(children: <Widget>[
-                Text('${_getTimeString(now)}', style: _timeStyle),
+                Text('${_getTimeString(now)}', style: timeStyle),
               ]),
               new Container(
                 padding:
-                    EdgeInsets.fromLTRB(_dateStyle.fontSize * 1, 0.0, 0.0, 0.0),
+                    EdgeInsets.fromLTRB(dateStyle.fontSize * 1, 0.0, 0.0, 0.0),
                 child: Row(children: <Widget>[
-                  new Text('晴，20~30°C', style: _weatherStyle)
+                  new Text(weather, style: weatherStyle)
                 ]),
               ),
             ],
           ),
         )));
+  }
+
+  startFetchWeather() async {
+    var result = '正在获取天气信息..';
+    setState(() => weather = result);
+
+    try {
+      result = await getWeatherString();
+      setState(() => weather = result);
+    } catch (e) {
+      setState(() => weather = '获取天气信息失败');
+    }
+    
+    new Timer(const Duration(hours: 1), () {
+      startFetchWeather();
+    });
   }
 
   String _getTimeString(now) {
